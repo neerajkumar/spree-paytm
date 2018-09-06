5e7e2f5a4c9d403f791f5071e78616ae3d518a12
Neeraj Kumar <neeraj.kumar@gmail.com>
2018-09-06 12:23:25 +0530

    adding paytm style button for payment

--- lib/generators/spree_paytm/install/install_generator.rb
+++ lib/generators/spree_paytm/install/install_generator.rb
@@ -4,6 +4,16 @@ module SpreePaytm
 
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
+      def add_stylesheets
+        if File.exists?('vendor/assets/stylesheets/spree/frontend/all.css')
+          inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/paytm\n", :before => /\*\//, :verbose => true
+        elsif File.exists?('vendor/assets/stylesheets/spree/frontend/all.css.scss')
+          append_file 'vendor/assets/stylesheets/spree/frontend/all.css.scss', "\n@import 'spree/paytm'\n"
+        elsif File.exists?('vendor/assets/stylesheets/spree/frontend/all.scss')
+          append_file 'vendor/assets/stylesheets/spree/frontend/all.scss', "\n@import 'spree/paytm'\n"
+        end
+      end
+
       def add_migrations
         run 'bundle exec rake railties:install:migrations FROM=spree_paytm'
       end
